dd0df2ae9fb2af0cca858e4f6a0e0f647ca84512
Zhen Xu <xuzhen165@gmail.com>
2018-09-24 00:28:44 -0400

    Started the CLI interface

--- cli.py
+++ cli.py
@@ -1,4 +1,27 @@
 import click
+import os
+from ml.base import MLConfig, MLEstimator
 
-def main():
-    pass
+_CONFIG_PATH = os.path.join(os.getcwd(), 'configs/')
+
+@click.command()
+@click.argument('action')
+@click.option('--config', help='The config file')
+def main(action, config):
+    settings = MLConfig.from_file(os.path.join(_CONFIG_PATH, config))
+    model = MLEstimator.from_config(settings)
+    def predict():
+        pass
+    def train():
+        pass
+    action_case = {
+        'train': train,
+        'predict': predict
+    }
+    try:
+        action_case[action]()
+    except KeyError:
+        MSG = 'The ACTION argument should be among'
+        for key in action_case.keys():
+            MSG + " '" + key + "' "
+        print(MSG)
