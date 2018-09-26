02d8e926dafab99a2952559a2f6d6fbaeeddcd58
Chengyu Dai <jdaaph@gmail.com>
2018-09-26 01:01:12 -0400

    change io

--- ml/mnist/io.py
+++ ml/mnist/io.py
@@ -1,12 +1,15 @@
-from ml.base import DefaultIO
+import tensorflow as tf
+
+from ml.base import KerasDatasetIO
 
 
-class MNIST(DefaultIO):
+class MNIST_Keras(KerasDatasetIO):
+    _NUM_CLASSES = 10
 
     @staticmethod
-    def parse_file(filename):
-        return data_chunk
+    def parse_iter(features, label):
+        '''Preprocess raw data to trainable input. '''
+        x = tf.reshape(tf.cast(features, tf.float32), (28, 28, 1))
+        y = tf.one_hot(tf.cast(label, tf.uint8), _NUM_CLASSES)
+        return x, y
 
-    @staticmethod
-    def parse_data(data_chunk):
-        return features, labels
