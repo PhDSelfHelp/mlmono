ac85303a673b7441bb49c14a1becff5d44244f2d
Chengyu Dai <jdaaph@gmail.com>
2018-09-27 13:59:20 -0400

    add not tested but logicaly complete squeezeseg io

--- ml/squeezeseg/io.py
+++ ml/squeezeseg/io.py
@@ -1,10 +1,16 @@
+from collections import defaultdict
+import itertools
 import logging
+import operator
 import os
 import subprocess
 import tensorflow as tf
 
 from ml.base import MLConfig, MLIO, TFRecordIO
+from ml.base.io_utils import create_dir_if_not_exist
 from ml.base.trainer_utils import is_training
+from ml.squeezeseg.io_utils import np_to_tfrecords
+
 
 
 _logger = logging.getLogger(name='KittiSqueezeSegIO')
@@ -14,6 +20,18 @@ class KittiSqueezeSegIO(TFRecordIO):
 
     DATASET_URL = "https://www.dropbox.com/s/pnzgcitvppmwfuf/lidar_2d.tgz"
     FULL_NUM_FILES = 10848
+    NUM_OF_DATA_GROUPS = 93
+    LABEL_TAG = 5
+    DEPTH_TAG = 5
+    DATA_SHAPE_X = (64, 512, 5)
+    DATA_SHAPE_Y = (64, 512, 1)
+
+    INPUT_MEAN = np.array([[[10.88, 0.23, -1.04, 0.21, 12.12]]])
+    INPUT_STD = np.array([[[11.47, 6.91, 0.86, 0.16, 12.32]]])
+
+    CLASSES = ['unknown', 'car', 'pedestrian', 'cyclist']
+    CLS_LOSS_WEIGHT = np.array([1/15.0, 1.0, 10.0, 10.0])
+    NUM_CLASS = len(KittiSqueezeSegIO.CLASSES)
 
     @staticmethod
     def download_data_if_not_exist(data_dir):
@@ -44,9 +62,37 @@ class KittiSqueezeSegIO(TFRecordIO):
             _logger.info(result.stdout.decode('utf-8'))
 
     @staticmethod
-    def parse_file(filename):
-        return None
+    def parse_file(example_proto):
+        features_format = {
+            'lidar_input': tf.FixedLenFeature([], tf.float32),
+            'lidar_mask' : tf.FixedLenFeature([], tf.float32),
+            'weight'     : tf.FixedLenFeature([], tf.float32),
+            'labels'     : tf.FixedLenFeature([], tf.float32),
+
+        }
+        parsed_features = tf.parse_single_example(example_proto, features_format)
+        features = {
+            'lidar_input'   : parsed_features['lidar_input'],
+            'lidar_mask'    : parsed_features['lidar_mask'],
+            'weight'        : parsed_features['weight'],
+        }
+        labels = tf.sparse_tensor_to_dense(parsed_features['labels'])
+
+        return features, labels
 
     @staticmethod
     def parse_iter(features, label):
         return features, label
+
+    def create_tf_record(self):
+        self.tfrecord_data_dir = os.join(self.data_dir, 'lidar_2d_tfrecords')
+        create_dir_if_not_exist(self.tfrecord_data_dir)
+        _logger.info("Creating tf records : ", self.tfrecord_data_dir)
+
+        numpy_fn_list = tf.gfile.Glob(os.join(self.data_dir, '*.npy'))
+
+        for fn in tqdm(numpy_fn_list):
+            outpath = os.join(self.tfrecord_data_dir, group + ".tfrecords")
+
+            X, Y = self.create_composite_numpy(fn_list)
+            np_to_tfrecords(X, Y, outpath)
--- ml/squeezeseg/io_utils.py
+++ ml/squeezeseg/io_utils.py
@@ -1,46 +1,107 @@
+import logging
+import numpy as np
 import tensorflow as tf 
 
-import numpy as np
+from ml.squeezeseg.io import KittiSqueezeSegIO
 
-import glob
+_logger = logging.getLogger('SqueezeSeg::io_utils')
 
-from PIL import Image
+def _group_numpy_fns(fn_list):
+    # example filename: "2011_09_26_0091_0000000062.npy"
+    #                          date_ idx_     frame.npy
+    groups = defaultdict(list)
+    for fn in fn_list:
+        group_idx = '_'.join(fn.split('_')[:3])
+        group[group_idx].append(fn)
+    return groups
 
-# Converting the values into features
-# _int64 is used for numeric values
+def _create_composite_numpy(fn_list):
+    X_list = []
+    Y_list = []
+    for fn in fn_list:
+        matrix = np.load(fn)
+        X_list.append(matrix[:, :, :KittiSqueezeSegIO.LABEL_TAG])
+        Y_list.append(matrix[:, :, KittiSqueezeSegIO.LABEL_TAG])
 
-def _int64_feature(value):
-    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+    composite_X = np.stack(X_list, axis=-1)
+    composite_Y = np.stack(Y_list, axis=-1)
+    return composite_X, composite_Y
 
-# _bytes is used for string/char values
+def np_to_tfrecords(X, file_path, verbose=False):
+    """ Taken from https://gist.github.com/swyoon/8185b3dcf08ec728fb22b99016dd533f.
+        Author: Sangwoong Yoon
 
-def _bytes_feature(value):
-    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+    Converts a Numpy array (or two Numpy arrays) into a tfrecord file.
+    For supervised learning, feed training inputs to X and training labels to Y.
+    For unsupervised learning, only feed training inputs to X, and feed None to Y.
+    The length of the first dimensions of X and Y should be the number of samples.
 
-tfrecord_filename = 'something.tfrecords'
+    Parameters
+    ----------
+    X : numpy.ndarray of rank 2
+        Numpy array for training inputs. Its dtype should be float32, float64, or int64.
+        If X has a higher rank, it should be rshape before fed to this function.
+    Y : numpy.ndarray of rank 2 or None
+        Numpy array for training labels. Its dtype should be float32, float64, or int64.
+        None if there is no label array.
+    file_path : str
+        The path and name of the resulting tfrecord file to be generated, with '.tfrecords'
+    verbose : bool
+        If true, progress is reported.
 
-# Initiating the writer and creating the tfrecords file.
+    Raises
+    ------
+    ValueError
+        If input type is not float (64 or 32) or int.
 
-writer = tf.python_io.TFRecordWriter(tfrecord_filename)
+    """
+    def _dtype_feature(ndarray):
+        """match appropriate tf.train.Feature class with dtype of ndarray. """
+        assert isinstance(ndarray, np.ndarray)
+        dtype_ = ndarray.dtype
+        if dtype_ == np.float64 or dtype_ == np.float32:
+            return lambda array: tf.train.Feature(float_list=tf.train.FloatList(value=array))
+        elif dtype_ == np.int64:
+            return lambda array: tf.train.Feature(int64_list=tf.train.Int64List(value=array))
+        else:
+            raise ValueError("The input should be numpy ndarray. \
+                              Instaed got {}".format(ndarray.dtype))
 
-# Loading the location of all files - image dataset
-# Considering our image dataset has apple or orange
-# The images are named as apple01.jpg, apple02.jpg .. , orange01.jpg .. etc.
+    assert isinstance(X, np.ndarray)
 
-images = glob.glob('data/*.jpg')
-for image in images[:1]:
-    img = Image.open(image)
-    img = np.array(img.resize((32,32)))
-label = 0 if 'apple' in image else 1
-feature = { 'label': _int64_feature(label),
-              'image': _bytes_feature(img.tostring()) }
+    # Load appropriate tf.train.Feature class depending on dtype.
+    dtype_feature_x = _dtype_feature(X)
 
-# Create an example protocol buffer
+    # Generate tfrecord writer.
+    writer = tf.python_io.TFRecordWriter(file_path)
+    if verbose:
+        msg = "Serializing {:d} examples into {}".format(X.shape[0], result_tf_file)
+        _logger.info(msg)
 
-example = tf.train.Example(features=tf.train.Features(feature=feature))
+    lidar_mask = np.reshape(
+        (X[:, :, KittiSqueezeSegIO.DEPTH_TAG] > 0), 
+        (self.zenith_level, self.azimuth_level, 1)
+    )
+    lidar_input = np.reshape(
+        X[:, :, :KittiSqueezeSegIO.LABEL_TAG], 
+        (self.zenith_level, self.azimuth_level, 1)
+    )
+    label = X[:, :, :KittiSqueezeSegIO.LABEL_TAG]
+    weight = np.zeros(label.shape)
+    for l in range(KittiSqueezeSegIO.NUM_CLASS):
+        weight[label==l] = KittiSqueezeSegIO.CLS_LOSS_WEIGHT[int(l)]
+    d_feature = {
+        'lidar_mask': dtype_feature_x(lidar_mask.ravel()),
+        'lidar_input': dtype_feature_x(lidar_input.ravel()),
+        'label': dtype_feature_x(label.ravel()),
+        'weight': dtype_feature_x(weight.ravel()),
+    }
 
-# Writing the serialized example.
+    features = tf.train.Features(feature=d_feature)
+    example = tf.train.Example(features=features)
+    serialized = example.SerializeToString()
+    writer.write(serialized)
+    writer.close()
 
-writer.write(example.SerializeToString())
-
-writer.close()
+    if verbose:
+        _logger.info("Writing {} done!".format(result_tf_file))
