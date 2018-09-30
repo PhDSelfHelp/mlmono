95977f6512be40ce92edfaf8f86d84843bbfec1d
Zhen Xu <xuzhen165@gmail.com>
2018-09-30 15:35:30 -0400

    Make the cli interface organized with cli.group

--- cli.py
+++ cli.py
@@ -2,25 +2,30 @@ import click
 from ml.base import MLConfig
 from ml.base.estimator import MLEstimator
 
+@click.group()
+def main():
+    pass
+
 @click.command()
-@click.argument('action')
 @click.option('--config', help='The config file')
-def main(action, config):
+def train(config):
     settings = MLConfig.from_file(config)
     # The command line action should override the one in config
-    settings.global_config.mode = action
+    settings.global_config.mode = 'train'
     model = MLEstimator.from_config(settings)
-    action_case = {
-        'train': model.train_fn,
-        'predict': model.predict_fn
-    }
-    try:
-        action_case[action]()
-    except KeyError:
-        msg = 'The ACTION argument should be among'
-        for key in action_case:
-            msg = msg + " '" + key + "' "
-        raise(ValueError(msg))
+    model.train_fn()
+
+@click.command()
+@click.option('--config', help='The config file')
+def predict(config):
+    settings = MLConfig.from_file(config)
+    # The command line action should override the one in config
+    settings.global_config.mode = 'predict'
+    model = MLEstimator.from_config(settings)
+    model.predict_fn()
+
+main.add_command(train)
+main.add_command(predict)
 
 if __name__ == "__main__":
     main()
