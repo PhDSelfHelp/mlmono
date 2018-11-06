dc873c548ebd91cb8acbc9c3045ab0efedc103d7
Chengyu Dai <jdaaph@gmail.com>
2018-11-05 22:09:58 -0500

    change steps

--- ml/base/estimator.py
+++ ml/base/estimator.py
@@ -22,7 +22,7 @@ class MLEstimator(object):
         print('enter training')
         self.estimator.train(
             input_fn = self.io.gen_input_fn(self.trainer.num_epochs),
-            steps=1000,
+            steps=self.config.num_epochs,
         )
 
     @classmethod
